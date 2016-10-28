ef3cbe9aead678ccce7a679e726d5b862e873eae
jefBinomed <jean.francois.garreau@gmail.com>
2016-10-29 00:47:42 +0200

    feat(#8) start including myo library code

--- scripts/prez/revealEngineEvents.js
+++ scripts/prez/revealEngineEvents.js
@@ -1,19 +1,40 @@
 'use strict'
 import {HighlightEvents} from './highlightEvents.js';
+import {MyoControler} from '../sensors/myoControler.js';
 
 export class RevealEngineEvents{
 	constructor(){
 	
 		let inIFrame = window.top != window.self;
 	
-    
+		this.myo = null;
 		if (!inIFrame){
 			// Management of actions in prez mode (not in preview mode)
+			this._myoBinding();
 		}
 
 		// In al case we init the highlight of code.
 		this._initHighlightCode();
 
+	}
+
+	_myoBinding(){
+		this.myo = new MyoControler();
+		let lastDoubleTap = 0;
+		document.getElementById('connectMyo').addEventListener('click', ()=>{
+			if (this.myo.connected){
+				this.myo.connect()
+				.then(()=>this.myo.init())
+				.then(()=>this.myo.registerGestures((gesture)=>{
+					if (gesture && gesture.gesture === 'double-tap'){
+						if(Date.now() - lastDoubleTap < 2000){
+							Reveal.next();
+						}
+						lastDoubleTap = Date.now();
+					} 
+				}));
+			}
+		});
 	}
 
 	_initHighlightCode() {
---
+++ scripts/sensors/myoControler.js
@@ -0,0 +1,141 @@
+'use strict'
+
+class MyoConfig{
+    constructor(){        
+    }
+    
+    controlService() { return "d5060001-a904-deb9-4748-2c7f4a124842" }
+    gestureService() { return "d5060003-a904-deb9-4748-2c7f4a124842" }
+    commandCharacteristic() { return "d5060401-a904-deb9-4748-2c7f4a124842" }
+    gestureCharacteristic() { return "d5060103-a904-deb9-4748-2c7f4a124842" }
+    
+
+}
+
+export class MyoController{
+    constructor(){
+        this.device = null;
+        this.config = new Config();
+        this.onDisconnected = this.onDisconnected.bind(this);
+        this.enableGesturesCommand = new Uint8Array(5);
+        enableGesturesCommand[0] = 0x01; // set mode
+        enableGesturesCommand[1] = 0x03; // bytes in payload
+        enableGesturesCommand[2] = 0x00; // emg mode: none
+        enableGesturesCommand[3] = 0x00; // imu mode: disabled
+        enableGesturesCommand[4] = 0x01; // classifier mode: enabled
+
+        this.disableGesturesCommand = Uint8Array.from(enableGesturesCommand);
+        disableGesturesCommand[4] = 0x00; // classifier mode: disabled
+
+        this.deepSleepCommand = new Uint8Array(2);
+        deepSleepCommand[0] = 0x04; // set mode
+        deepSleepCommand[1] = 0x00; // bytes in payload
+
+        this.connected = false;
+    }
+
+    /*
+    Request the device with bluetooth
+    */
+    request() {
+        let options = {
+            "filters": [{
+                "services": [this.config.controlService()]
+            }],
+            "optionalServices": [this.config.gestureService()]
+        };        
+        return navigator.bluetooth.requestDevice(options)
+            .then(device => {
+                this.device = device;
+                this.device.addEventListener('gattserverdisconnected', this.onDisconnected);
+                return device;
+            });
+    }
+
+    /**
+     * Connect to the device
+     * */
+    connect() {
+        if (!this.device) {
+            return Promise.reject('Device is not connected.');
+        } else {
+            this.connected = true;
+            return this.device.gatt.connect();
+        }
+    }
+    
+    init(){
+        if(!this.device){
+            return Promise.reject('Device is not connected.');
+        }else{
+            
+            return this.device.gatt.getPrimaryService(this.config.controlService())
+            .then((service)=>{
+                 console.log('> get Myo Control Service');
+                 return service.getCharacteristic(this.config.commandCharacteristic())
+            })
+            .then((characteristic)=>{
+                  console.log('> get Myo Command characteristic');
+                  return characteristic.writeValue(this.enableGesturesCommand)
+            })
+            .then(()=>{
+                console.log('Ready to listen gestures');
+            })
+            .catch(error => console.error(error))
+        }
+    }
+
+    registerGestures(callback){
+        if(!this.device){
+            return Promise.reject('Device is not connected.');
+        }else{
+            this.device.gatt.getPrimaryService(this.config.gestureService())
+            .then(service=>{
+                console.log('> Get Gesture Service');
+                return service.getCharacteristic(this.config.gestureCharacteristic())
+            })
+            .then((characteristic) => {
+                console.log('Get gesture caracteristic')
+                characteristic.startNotifications();
+                characteristic.addEventListener('characteristicvaluechanged', (ev) => {
+                    const gesture = this._parseMyoGesture(ev.target.value);
+                    console.log('Gesture : ', gesture);
+                    if (callback){
+                        callback(gesture);
+                    }
+                })
+            })
+            .catch(error => console.error(error));
+        }
+    }
+
+     disconnect() {
+        if (!this.device) {
+            return Promise.reject('Device is not connected.');
+        } else {
+            return this.device.gatt.disconnect();
+        }
+    }
+
+    onDisconnected() {
+        this.connected = false;
+        console.log('Device is disconnected.');
+    }
+
+    _parseMyoGesture(value) {
+        if (value.getUint8(0) === 0x03) {
+            const gestureValue = value.getUint16(1, true)
+            const gesture = {
+                0x0000: 'rest',
+                0x0001: 'fist',
+                0x0002: 'wave-in',
+                0x0003: 'wave-out',
+                0x0004: 'fingers-spread',
+                0x0005: 'double-tap',
+                0xffff: 'unknown',
+            }[gestureValue]
+            return { gesture }
+        }
+        return { gesture: null }
+    }
+}

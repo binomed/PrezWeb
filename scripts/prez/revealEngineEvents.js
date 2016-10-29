f6b94ab3f1fdbecb568d38d9014a750d29964d0f
jefBinomed <jean.francois.garreau@gmail.com>
2016-10-29 23:12:19 +0200

    Move myo script to webbluetooth directory

--- scripts/prez/revealEngineEvents.js
+++ scripts/prez/revealEngineEvents.js
@@ -1,16 +1,16 @@
 'use strict'
 import {HighlightEvents} from './highlightEvents.js';
-import {MyoControler} from '../sensors/myoControler.js';
+import {BlePrezControler} from '../sensors/blePrezControler.js';
+
 
 export class RevealEngineEvents{
 	constructor(){
 	
 		let inIFrame = window.top != window.self;
-	
-		this.myo = null;
+		
 		if (!inIFrame){
 			// Management of actions in prez mode (not in preview mode)
-			this._myoBinding();
+			new BlePrezControler();
 		}
 
 		// In al case we init the highlight of code.
@@ -18,24 +18,7 @@ export class RevealEngineEvents{
 
 	}
 
-	_myoBinding(){
-		this.myo = new MyoControler();
-		let lastDoubleTap = 0;
-		document.getElementById('connectMyo').addEventListener('click', ()=>{
-			if (this.myo.connected){
-				this.myo.connect()
-				.then(()=>this.myo.init())
-				.then(()=>this.myo.registerGestures((gesture)=>{
-					if (gesture && gesture.gesture === 'double-tap'){
-						if(Date.now() - lastDoubleTap < 2000){
-							Reveal.next();
-						}
-						lastDoubleTap = Date.now();
-					} 
-				}));
-			}
-		});
-	}
+	
 
 	_initHighlightCode() {
 
